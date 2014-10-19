afafd7643511e8bbb8016780568a7968e4cd0bb5
Zachary Huff <zach.huff.386@gmail.com>
2014-10-19 15:11:31 -0400

    Fix constants

--- pritunl_loader/constants.py
+++ pritunl_loader/constants.py
@@ -19,7 +19,7 @@ OAUTH_CLIENT_ID = os.getenv('OAUTH_CLIENT_ID', None)
 OAUTH_CLIENT_SECRET = os.getenv('OAUTH_CLIENT_SECRET', None)
 OAUTH_REDIRECT_URI = os.getenv('OAUTH_REDIRECT_URI',
     'http://localhost:8500/oauth')
-OAUTH_REDIRECT_HOME_URI = os.getenv('OAUTH_REDIRECT_URI',
+OAUTH_REDIRECT_HOME_URI = os.getenv('OAUTH_REDIRECT_HOME_URI',
     'http://localhost:4000')
 REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
 ORIGIN_URL = os.getenv('ORIGIN_URL', 'http://localhost:4000')
